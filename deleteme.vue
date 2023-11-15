1693e1572618dfb8067a75dab42258959b2e65e0
kevinwkc <kevinwkc@gmail.com>
2023-11-15 02:41:14 -0500

    redeploy

---
+++ deleteme.vue
@@ -0,0 +1,24 @@
+methods: {
+  ...mapActions(['randInt'])
+},
+
+import axios from 'axios'
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
+import {app} from 'main'
+
+// Create a new store instance.
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+app.use(store)
